a74d0ad035d4d76ef9ade4e976ac62d80662de9b
Yatish <yatishjariyal@gmail.com>
2021-01-11 21:56:28 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
-import Header from './components/Header'
 import Home from "./components/Home"
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import Footer from './components/Footer'
 import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
       <Home/>
       <About />
       <Skills />
       <Projects/>
       <Contact />
-      {/* <Footer/> */}
     </div>
   );
 }
